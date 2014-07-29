f67ad42171b3ad245f7cbf5822461efe969f595c
carfm <fuentes.carlos4@gmail.com>
2014-07-28 20:37:56 -0600

    arreglo en algunas vistas y agregado bean de etl

--- SIGOAP/src/java/sv/com/hmcr/negocio/beans/EtlBean.java
+++ SIGOAP/src/java/sv/com/hmcr/negocio/beans/EtlBean.java
@@ -3,9 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package sv.com.hmcr.negocio.beans;
 
 import java.awt.event.ActionEvent;
+import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import sv.com.hmcr.dao.TablasTempDAO;
@@ -14,25 +16,23 @@ import sv.com.hmcr.dao.TablasTempDAO;
  *
  * @author Carlos y Jose
  */
-@ManagedBean(name = "etlBean")
+@ManagedBean
 @ViewScoped
-public class EtlBean {
+public class EtlBean implements Serializable{
 
     /**
      * Creates a new instance of EtlBean
      */
+    
     private TablasTempDAO procedimiento;
-    private Integer progress;
-
+    
     public EtlBean() {
-        progress = 0;
         procedimiento = new TablasTempDAO();
     }
-
-    public void etl(ActionEvent action) {
-        progress = 20;
-        procedimiento.ejecutarProc("call etlEmpleado()");
-        progress = 100;
+    
+    public void etl(ActionEvent actionEvent){
+        getProcedimiento().ejecutarProc("call etlEmpleados()");
+        
     }
 
     /**
@@ -48,26 +48,5 @@ public class EtlBean {
     public void setProcedimiento(TablasTempDAO procedimiento) {
         this.procedimiento = procedimiento;
     }
-
-    /**
-     * @return the progress
-     */
-    public Integer getProgress() {
-//        if (progress == null) {
-//            progress = 0;
-//        } else {
-//            progress = progress + (int) (Math.random() * 35);
-//
-//            if (progress > 100) {
-//                progress = 100;
-//            }
-//        }
-
-        return progress;
-    }
-
-    public void setProgress(Integer progress) {
-        this.progress = progress;
-    }
-
+    
 }
