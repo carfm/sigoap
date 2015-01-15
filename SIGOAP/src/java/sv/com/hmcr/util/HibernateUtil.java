7b686e79394e2d710478599e71887413f3c2f419
carfm <fuentes.carlos4@gmail.com>
2015-01-14 23:23:21 -0600

    Cambios para manejo de Hibernate.Util

--- SIGOAP/src/java/sv/com/hmcr/util/HibernateUtil.java
+++ SIGOAP/src/java/sv/com/hmcr/util/HibernateUtil.java
@@ -5,51 +5,68 @@
  */
 package sv.com.hmcr.util;
 
-import org.hibernate.Session;
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.context.internal.ThreadLocalSessionContext;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
 
-public class HibernateUtil implements java.io.Serializable {
+public class HibernateUtil {
 
-    private static SessionFactory sessionFactory;
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
-    public static synchronized void buildSessionFactory() {
-        if (sessionFactory == null) {
-            Configuration configuration = new Configuration();
-            configuration.configure();
-            configuration.setProperty("hibernate.current_session_context_class", "thread");
-            ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
-            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch (HibernateException he) {
+            System.err.println("Ocurrió un error en la inicialización de la SessionFactory: " + he);
+            throw new ExceptionInInitializerError(he);
         }
-    }
-
-    public static void openSessionAndBindToThread() {
-        Session session = sessionFactory.openSession();
-        ThreadLocalSessionContext.bind(session);
     }
 
     public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            buildSessionFactory();
-        }
         return sessionFactory;
     }
 
-    public static void closeSessionAndUnbindFromThread() {
-        Session session = ThreadLocalSessionContext.unbind(sessionFactory);
-        if (session.isConnected()) {
-            session.close();
-        }
-    }
-
-    public static void closeSessionFactory() {
-        if ((sessionFactory != null) && (sessionFactory.isClosed() == false)) {
-            sessionFactory.close();
-        }
-    }
+//    private static SessionFactory sessionFactory;
+//
+//    public static synchronized void buildSessionFactory() {
+//        try {
+//            if (sessionFactory == null) {
+//                Configuration configuration = new Configuration();
+//                configuration.configure();
+//                configuration.setProperty("hibernate.current_session_context_class", "thread");
+//                ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+//                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+//            }
+//        } catch (Exception e) {
+//            System.out.println("ERRORES DE CONEXION");
+//        }
+//
+//    }
+//
+//    public static void openSessionAndBindToThread() {
+//        Session session = sessionFactory.openSession();
+//        ThreadLocalSessionContext.bind(session);
+//    }
+//
+//    public static SessionFactory getSessionFactory() {
+//        if (sessionFactory == null) {
+//            buildSessionFactory();
+//        }
+//        return sessionFactory;
+//    }
+//
+//    public static void closeSessionAndUnbindFromThread() {
+//        Session session = ThreadLocalSessionContext.unbind(sessionFactory);
+//        if (session.isConnected()) {
+//            session.close();
+//        }
+//    }
+//
+//    public static void closeSessionFactory() {
+//        if ((sessionFactory != null) && (sessionFactory.isClosed() == false)) {
+//            sessionFactory.close();
+//        }
+//    }
 //    private static SessionFactory sessionFactory;
 //    private static ServiceRegistry serviceRegistry;
 //
